8bb5cb2b17581ebdaddf0652c539ed4691f31359
mk-pmb <mk-pmb@users.noreply.github.com>
2023-04-15 21:48:49 +0200

    Try to browserify on GitHub Actions

---
+++ build/compile.js
@@ -0,0 +1,37 @@
+﻿/* -*- tab-width: 2 -*- */
+'use strict';
+
+require('p-fatal');
+
+const browserify = require('browserify');
+const brCfg = {
+  node: true,
+  sourceType: 'module',
+  'extensions': [
+    '.mjs',
+    '.jsm',
+    '.js',
+  ],
+  'debug': true,
+  'plugins': [
+    'esmify',
+  ],
+  'transform': [
+    [require.resolve('babelify'),
+      { 'presets': [require.resolve('@babel/preset-env')] }],
+    [require.resolve('envify'), { '_': 'purge' }],
+    [require.resolve('brfs')],
+  ],
+  'entries': [require.resolve('../src/cli.mjs')],
+};
+
+browserify(brCfg).bundle(function cb(err, buf) {
+  if (!err) { return console.info('Success:', buf); }
+  const msg = String(err);
+  const abbreviate = (
+    msg.startsWith('SyntaxError: Unexpected token (')
+    || msg.startsWith("Error: Can't walk dependency graph: ")
+  );
+  console.error(abbreviate ? msg : err);
+  process.exit(5);
+});
---
+++ build/depsHelper.js
@@ -0,0 +1,12 @@
+﻿'use strict';
+
+// This file is meant as a hint to `guess-js-deps-bash`.
+
+require('@babel/core');
+require('@babel/preset-env');
+require('@babel/preset-stage-0');
+require('babelify');
+require('brfs');
+require('browser-resolve');
+require('envify');
+require('esmify');
